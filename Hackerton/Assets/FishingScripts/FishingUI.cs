95f48d5811f5ad7d41cda347c7cbe413c2b9f9c9
hyechxn <a49918072@gmail.com>
2024-10-30 11:03:17 +0900

    Fishing

---
+++ Hackerton/Assets/FishingScripts/FishingUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FishingUI : MonoBehaviour
+{
+    public Image hpFill;
+
+    public FishingCharacter fishingCharacter;
+
+    public bool isEnd;
+
+    private void Start()
+    {
+        StartCoroutine(Minus());
+    }
+
+    void FixedUpdate()
+    {
+        hpFill.fillAmount = Mathf.Lerp(fishingCharacter.HP / 10f, hpFill.fillAmount, 0.0002f * Time.deltaTime);
+
+    }
+
+    IEnumerator Minus()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            fishingCharacter.HP -= 0.5f;
+
+            if (fishingCharacter.HP <= 0)
+                break;
+        }
+
+        isEnd = true;
+    }
+}
