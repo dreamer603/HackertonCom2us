e87cb03e811ab176cc1f569204cf187298055ede
hyechxn <a49918072@gmail.com>
2024-10-30 14:23:55 +0900

    Fish Change

--- Hackerton/Assets/Fishmove2.cs
+++ Hackerton/Assets/Fishmove2.cs
@@ -2,18 +2,27 @@ using UnityEngine;
 
 public class Fishmove2 : MonoBehaviour
 {
-    public float speed = 1.0f; // 초기 물고기 이동 속도
-    public float acceleration = 0.1f; // 초당 증가할 속도
+    public float baseInitialSpeed = 1.0f; 
+    public float speedIncrement = 0.05f;
+    private static float nextInitialSpeed;
+
+    private float speed; 
+
+    private void Awake()
+    {
+        if (nextInitialSpeed == 0)
+        {
+            nextInitialSpeed = baseInitialSpeed;
+        }
+
+        speed = nextInitialSpeed;
+        nextInitialSpeed += speedIncrement;
+    }
 
     private void Update()
     {
-        // 초당 속도 증가
-        speed += acceleration * Time.deltaTime* -1f;
-
-        // 증가된 속도로 X 축 방향으로 이동
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
-
-        // 속도 확인용 출력
+        // 현재 속도로 이동
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
         Debug.Log("Current Speed: " + speed);
     }
 }
